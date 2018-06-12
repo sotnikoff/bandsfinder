2250b564a33d47497715285e9b231d1c2541d9ae
Unknown <thehofch@gmail.com>
2018-06-12 21:33:25 +0300

    add pending specs

---
+++ spec/controllers/musicians_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MusiciansController, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ProfilesController, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
