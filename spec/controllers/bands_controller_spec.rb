8dac30653e3497551f29d15c63abc4ce240040f0
Unknown <thehofch@gmail.com>
2018-06-12 23:00:25 +0300

    bands specs

--- spec/controllers/bands_controller_spec.rb
+++ spec/controllers/bands_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe BandsController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:band) { create :band }
+
+  describe 'GET #index' do
+    it 'returns 200' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns 200' do
+      get :show, params: { id: band }
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns 200' do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    it 'returns :found' do
+      musician = create :musician
+      sign_in musician.user
+      post :create
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
