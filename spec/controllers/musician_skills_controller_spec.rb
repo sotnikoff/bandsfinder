b6a847540a77f408f55a2709c9963a8d43f02fea
Unknown <thehofch@gmail.com>
2018-06-12 23:22:56 +0300

    musician skill spec

--- spec/controllers/musician_skills_controller_spec.rb
+++ spec/controllers/musician_skills_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe MusicianSkillsController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'POST #create' do
+    it 'returns :found' do
+      musician_skill = build :musician_skill
+      post :create, params: {
+        musician_id: musician_skill.musician.id,
+        skill_id: musician_skill.skill.id,
+        comment: musician_skill.comment
+      }
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'returns :found' do
+      musician_skill = create :musician_skill
+      delete :destroy, params: {
+          id: musician_skill.id,
+          musician_id: musician_skill.musician.id
+      }
+    end
+  end
 end
---
+++ spec/factories/musician_skill.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :musician_skill do
+    musician
+    skill
+  end
+end
