dc33a2af1b68be5d6492e5df4ce504c614262618
Unknown <thehofch@gmail.com>
2018-06-13 02:32:31 +0300

    profile + search

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ProfilesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = create :user
+    sign_in @user
+  end
+
+  describe 'GET #show' do
+    it 'returns 200' do
+      get :show, params: { id: @user.id }
+      expect(response).to be_successful
+    end
+  end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = create :user
+    sign_in @user
+  end
+
+  describe 'GET #musicians' do
+    it 'returns 200' do
+      get :musicians
+      expect(response).to be_successful
+    end
+  end
 end
