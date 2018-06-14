9856ff3661bdc8dc3c9aa83d18483f51297f05e2
Unknown <thehofch@gmail.com>
2018-06-14 10:56:07 +0300

    fix operations

--- app/operations/approve_band_request.rb
+++ app/operations/approve_band_request.rb
@@ -4,8 +4,8 @@ module ApproveBandRequest
   def call(band_request)
     return nil unless band_request.status_new?
     band_request.status_approved!
-    band = @band_request.band
-    musician = @band_request.musician
+    band = band_request.band
+    musician = band_request.musician
     band.musicians << musician unless band.musicians.exists?(musician.id)
   end
 end
