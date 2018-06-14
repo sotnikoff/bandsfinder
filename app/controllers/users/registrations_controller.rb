8b554c1b78f2d66ea05cdda8b2191fc94cdf4777
Unknown <thehofch@gmail.com>
2018-06-14 15:33:53 +0300

    user edit links + layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -34,9 +34,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    render :edit, layout: 'application'
+  end
 
   # PUT /resource
   # def update
