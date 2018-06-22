518a07f9596885a686d2dc27954d97721688d750
Unknown <thehofch@gmail.com>
2018-06-22 13:05:34 +0300

    var fix + added image resizing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def user_place
-    "#{current_user.city.region.country.title}, "\
-    "#{current_user.city.region.title}, #{current_user.city.title}"
+  def user_place(user)
+    "#{user.city.region.country.title}, "\
+    "#{user.city.region.title}, #{user.city.title}"
   end
 end
