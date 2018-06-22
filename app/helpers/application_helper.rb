6f146b1149a1cbb23606dd2450428833d6ceb4c5
Unknown <thehofch@gmail.com>
2018-06-22 08:30:24 +0300

    code style fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def user_place
-    "#{current_user.city.region.country.title}, #{current_user.city.region.title}, #{current_user.city.title}"
+    "#{current_user.city.region.country.title}, "\
+    "#{current_user.city.region.title}, #{current_user.city.title}"
   end
 end
