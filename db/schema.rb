2e5b361d5909d8ea88afc7009b653e51d479cb26
Unknown <thehofch@gmail.com>
2018-06-25 20:09:55 +0300

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_141837) do
+ActiveRecord::Schema.define(version: 2018_06_25_160910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2018_06_22_141837) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_musicians_on_user_id"
   end
 
